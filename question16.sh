45542bf39c3f64df0dc31fac16dd06cf4103fc6b
Jake <jake@Jakes-MacBook-Pro.local>
2024-09-15 22:35:21 +0930

    COMMITXXX

--- question16.sh
+++ question16.sh
@@ -1,8 +1,9 @@
 git clone https://github.com/ian-knight-uofa/git-practice-02.git
 cd git-practice-02
 git fetch origin
-git checkout -b branch3 origin/branch3
+git checkout branch3
 git checkout branch2
+git fetch origin 
 git merge branch3
 git branch -d branch3
 git commit -m "COMMIT"
