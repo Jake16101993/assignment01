98861fb96fac30e16b3639fa80af7fa53048f007
Jake <jake@Jakes-MacBook-Pro.local>
2024-09-15 17:29:15 +0930

    Add question18.sh is DONE

---
+++ question18.sh
@@ -0,0 +1,12 @@
+git clone https://github.com/ian-knight-uofa/git-practice-04.git
+cd git-practice-04
+
+git branch -a
+
+git checkout main
+git merge ready*
+
+git branch -d ready*
+
+git checkout update*
+git push origin update*
