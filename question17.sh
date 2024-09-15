abe8ba7c9f9123f71751801016a59b9f34ed1776
Jake <jake@Jakes-MacBook-Pro.local>
2024-09-15 19:47:50 +0930

    COMMIT3

--- question17.sh
+++ question17.sh
@@ -1,11 +1,6 @@
 git clone https://github.com/ian-knight-uofa/git-practice-03.git
-
+cd git-practice-03
 git checkout main
-mkdir dir1
-mkdir dir1/dir2
-touch dir1/dir2/foo
-mkdir dir3
-touch dir3/bar
 cp dir3/bar dir3/bar_copy
 touch LICENSE
 touch README.md
@@ -26,9 +21,8 @@ git checkout -b branch2
 rm dir1/foo
 mkdir dir1/dir2
 touch dir1/dir2/foo_modified
-rm dir3/bar
+mv dir3/bar dir3/newfile2
 mv dir3 dir1/
-touch dir1/dir3/newfile2
 rm newfile1
 
 git add .
