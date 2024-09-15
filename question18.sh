bf4b810b7ef2f5b7ea889fb13c50740d0b39cf77
Jake <jake@Jakes-MacBook-Pro.local>
2024-09-15 22:45:05 +0930

    COMMITXXX

--- question18.sh
+++ question18.sh
@@ -2,28 +2,31 @@ git clone https://github.com/ian-knight-uofa/git-practice-04.git
 cd git-practice-04
 git branch -a
 
-git checkout main
-git fetch origin
-git checkout -b ready1 origin/ready1
-git checkout -b ready2 origin/ready2
-git checkout -b ready3 origin/ready3
-git checkout -b update1 origin/update1
-git checkout -b update2 origin/update2
+git checkout ready1
+git checkout ready2
+git checkout ready3
 
 git checkout main
 
+git fetch origin  
 git merge ready1
+git fetch origin  
 git merge ready2
+git fetch origin  
 git merge ready3
 
 git branch -d ready1
 git branch -d ready2
 git branch -d ready3
 
+git checkout main
+git pull origin main 
+
 git checkout update1
 git merge main
-git push origin update1
 
 git checkout update2
 git merge main
-git push origin update2
+
+git add .
+git commit -m "COMMIT"
