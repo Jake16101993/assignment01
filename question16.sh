d75ca6cf3966ba40d8ba975f7485d98185fc6d2b
Jake <jake@Jakes-MacBook-Pro.local>
2024-09-15 21:41:30 +0930

    COMMITx

--- question16.sh
+++ question16.sh
@@ -1,7 +1,7 @@
 git clone https://github.com/ian-knight-uofa/git-practice-02.git
 cd git-practice-02
+git fetch origin
 git checkout branch2
-git add dir1/file2
 git merge origin/branch3
 git branch -d branch3
 git commit -m "COMMIT"
