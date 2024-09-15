2599bc17cda154c8c23a70d48b2e687e98be7740
Jake <jake@Jakes-MacBook-Pro.local>
2024-09-15 18:51:55 +0930

    COMMIT2

--- question16.sh
+++ question16.sh
@@ -1,5 +1,4 @@
-git clone https://github.com/ian-knight-uofa/git-practice-01.git
-cd git-practice-01
+git clone https://github.com/ian-knight-uofa/git-practice-02.git
 git checkout branch2
 git merge branch3
 git branch -d branch3
